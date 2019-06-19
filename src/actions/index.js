884d82d738d5d379e05e3ce54f27de4b7c77fc9e
Dave Irwin <david@redirwin.com>
2019-06-19 15:49:06 -0700

    Created and exporting action types. Added error payload.

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,10 @@ import axios from "axios";
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
 
+export const FETCH_CHARS_START = "FETCH_CHARS_START";
+export const FETCH_CHARS_SUCCESS = "FETCH_CHARS_SUCCESS";
+export const FETCH_CHARS_FAILURE = "FETCH_CHARS_FAILURE";
+
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
@@ -15,6 +19,6 @@ export function getCharacters() {
       .then(res => {
         dispatch({ type: FETCH_CHARS_SUCCESS, payload: res.chars });
       })
-      .catch(error => dispatch({ type: FETCH_CHARS_FAILURE }));
+      .catch(error => dispatch({ type: FETCH_CHARS_FAILURE, payload: error }));
   };
 }

c39bfbede5c594b97f316bdeb42e935a8a903894
Dave Irwin <david@redirwin.com>
2019-06-19 18:58:22 -0700

    Removed unneeded comments.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,8 @@
 import axios from "axios";
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for success and one for failure
-
 export const FETCH_CHARS_START = "FETCH_CHARS_START";
 export const FETCH_CHARS_SUCCESS = "FETCH_CHARS_SUCCESS";
 export const FETCH_CHARS_FAILURE = "FETCH_CHARS_FAILURE";
-
-// our action creator will be a function that returns a function
-// the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
 
 export function getCharacters() {
   return dispatch => {
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,7 +2,6 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
-// import actions
 
 class CharacterListView extends React.Component {
   constructor() {

5e5f92fd44f2a30ece29341a2ef154de0a5e3c47
Dave Irwin <david@redirwin.com>
2019-06-19 19:36:28 -0700

    Created mapStateToProps and passed into connect with getCharacters action.

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -4,21 +4,27 @@ import { getCharacters } from "../actions";
 import { CharacterList } from "../components";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
+  // constructor() {
+  //   super();
+  // }
 
   componentDidMount() {
     this.props.dispatch(getCharacters());
   }
 
   render() {
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
+    console.log(this.props);
+    const { error, loading, characters } = this.props;
+    if (loading) {
+      return <div>Loading characters...</div>;
+    }
+
+    if (error) {
+      return <div>Sorry, but something went wrong. {error.message}</div>;
     }
     return (
       <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />
+        <CharacterList characters={characters} />
       </div>
     );
   }
@@ -26,9 +32,14 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+
+const mapStateToProps = state => ({
+  characters: state.characters,
+  loading: state.loading,
+  error: state.error
+});
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps,
+  { getCharacters }
 )(CharacterListView);
